fdff4628626ea98b70120cf73ad07c344f15a13b
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-19 10:42:36 +0000

    Palos cambian aleatoriamente al cargar la página

--- src/app.js
+++ src/app.js
@@ -8,4 +8,12 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const cardClasses = ["♦", "♥", "♠", "♣"];
+  const randomIndex = Math.floor(Math.random() * cardClasses.length);
+  const randomContent = document.createTextNode(cardClasses[randomIndex]);
+
+  let topCard = document.querySelector(".top");
+  topCard.textContent = randomContent.textContent;
+  let bottomCard = document.querySelector(".bottom");
+  bottomCard.textContent = randomContent.textContent;
 };
