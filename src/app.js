edb5fcc86d1750be517825cca1db470c62c4cc91
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-19 11:25:25 +0000

    Numeros cambian aleatoriamente al refrescar la página. Falta ajustar la imagen y agregar la que falta

--- src/app.js
+++ src/app.js
@@ -16,4 +16,31 @@ window.onload = function() {
   topCard.textContent = randomContent.textContent;
   let bottomCard = document.querySelector(".bottom");
   bottomCard.textContent = randomContent.textContent;
+
+  if (randomIndex === 0 || randomIndex === 1) {
+    topCard.classList.add("red-text");
+    bottomCard.classList.add("red-text");
+  } else {
+    topCard.classList.add("black-text");
+    bottomCard.classList.add("black-text");
+  }
+
+  const cardNumbers = [
+    Math.floor(Math.random() * 9) + 2,
+    '<img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSHdR-DiLOvADl0me4JGJOhsPZEgNVMogG53a48eNi8W3DanH72">',
+    "J",
+    "A"
+  ];
+  const indexNumber = Math.floor(Math.random() * cardNumbers.length);
+  const randomNumber = document.createTextNode(cardNumbers[indexNumber]);
+
+  let numberCard = document.querySelector(".number");
+  if (indexNumber === 1) {
+    let imgElement = document.createElement("img");
+    imgElement.src =
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSHdR-DiLOvADl0me4JGJOhsPZEgNVMogG53a48eNi8W3DanH72";
+    numberCard.appendChild(imgElement);
+  } else {
+    numberCard.textContent = randomNumber.textContent;
+  }
 };
