ed47b4e41b415aa127c626a134bacf70ca0a3693
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-21 22:33:35 +0000

    Cambio de código. Creé una función aparte para el contenido para poder ser reutilizada. Organización diferente. Shuffle al hacer click en el botón

--- src/app.js
+++ src/app.js
@@ -5,6 +5,58 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let myShuffleCards = document.getElementById("shuffleCards");
+
+function theContentOfTheCards() {
+  //Las imagenes para poder renderizarlas más fácilmente
+  let theQueen = document.createElement("img");
+  theQueen.src = "https://i.postimg.cc/d0qVV7pg/reina.jpg";
+  theQueen.classList.add("img-modificada");
+
+  let theKing = document.createElement("img");
+  theKing.src = "https://i.postimg.cc/vm3HgYdT/rey3-1.jpg";
+  theKing.classList.add("img-modificada");
+
+  //El objeto con los arrays de contenido
+  let myCard = {
+    suit: ["♦", "♥", "♠", "♣"],
+    numbers: [Math.floor(Math.random() * 9) + 2, "J", "A", "queen", "king"]
+  };
+
+  //Genero un index random para elegir un contenido random en cada array
+  let indexSuit = Math.floor(Math.random() * myCard.suit.length);
+  let suitContent = myCard.suit[indexSuit];
+  let indexNumbers = Math.floor(Math.random() * myCard.numbers.length);
+  let numbersContent = myCard.numbers[indexNumbers];
+
+  //Declaro los elementos top y bottom para los iconos y le agrego el contenido random anterior
+  let topCard = document.getElementById("topCard");
+  topCard.textContent = suitContent;
+  let bottomCard = document.getElementById("bottomCard");
+  bottomCard.textContent = suitContent;
+  if (indexSuit === 0 || indexSuit === 1) {
+    topCard.style.color = "red";
+    bottomCard.style.color = "red";
+  } else {
+    topCard.style.color = "black";
+    bottomCard.style.color = "black";
+  }
+
+  //Declaro el elemento del medio donde irá el contenido. Utilizo el condicional para agregar de forma específica si es una imagen.
+  let middleCard = document.querySelector(".number");
+  middleCard.innerHTML = "";
+  if (indexNumbers === 3) {
+    middleCard.appendChild(theQueen.cloneNode());
+  } else if (indexNumbers === 4) {
+    middleCard.appendChild(theKing.cloneNode());
+  } else {
+    middleCard.textContent = numbersContent;
+  }
+}
+window.onload = theContentOfTheCards;
+myShuffleCards.addEventListener("click", theContentOfTheCards);
+
+/* CÓDIGO ANTERIOR. EL DE ARRIBA ES EL MEJORADO
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
@@ -50,3 +102,22 @@ window.onload = function() {
     numberCard.textContent = randomNumber.textContent;
   }
 };
+
+window.addCard = function addCard() {
+  let newCard = document.createElement("div");
+  newCard.classList.add("card");
+  newCard.classList.add("theNewCard");
+  let topCard = document.createElement("div");
+  topCard.classList.add("top");
+  let numberCard = document.createElement("div");
+  numberCard.classList.add("number");
+  let bottomCard = document.createElement("div");
+  bottomCard.classList.add("bottom");
+
+  newCard.appendChild(topCard);
+  newCard.appendChild(numberCard);
+  newCard.appendChild(bottomCard);
+
+  document.body.appendChild(newCard);
+};
+*/
